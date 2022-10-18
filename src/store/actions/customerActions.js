e2c481e5c70a259114a3ec495f76afc113329fa6
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-18 22:04:11 +0300

    Create customer actions

---
+++ src/store/actions/customerActions.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../../api/apiRequests';
+import { hideNotification, showNotification } from '../reducers/uiReducers';
+
+const fetchCustomers = createAsyncThunk(
+  'customer/fetchCustomers',
+  async (thunkAPI) => {
+    try {
+      const response = await api.get('/customers', { withCredentials: true });
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const createCustomer = createAsyncThunk(
+  'customer/createCustomer',
+  async (customer, thunkAPI) => {
+    try {
+      const response = await api.post('/customers', customer, { withCredentials: true });
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const updateCustomer = createAsyncThunk(
+  'customer/updateCustomer',
+  async (customer, thunkAPI) => {
+    try {
+      const response = await api.put(`/customers/${customer.id}`, customer, { withCredentials: true });
+      thunkAPI.dispatch(showNotification(
+        { message: response.data.message, isError: false, isOpen: true },
+      ));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const deleteCustomer = createAsyncThunk(
+  'customer/deleteCustomer',
+  async (id, thunkAPI) => {
+    try {
+      const response = await api.delete(`/customers/${id}`, { withCredentials: true });
+      thunkAPI.dispatch(showNotification(
+        { message: response.data.message, isError: false, isOpen: true },
+      ));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+export {
+  fetchCustomers, createCustomer, updateCustomer, deleteCustomer,
+};

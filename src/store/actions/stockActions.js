86333a3d21d4249d571e00cd6adb4fcf6c04a953
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-18 22:43:26 +0300

    Create stock actions

---
+++ src/store/actions/stockActions.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../../api/apiRequests';
+import { hideNotification, showNotification } from '../reducers/uiReducers';
+
+const fetchStocks = createAsyncThunk(
+  'stock/fetchStocks',
+  async (thunkAPI) => {
+    try {
+      const response = await api.get('/stocks', { withCredentials: true });
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const createStock = createAsyncThunk(
+  'stock/createStock',
+  async (stock, thunkAPI) => {
+    try {
+      const response = await api.post('/stocks', stock, { withCredentials: true });
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const updateStock = createAsyncThunk(
+  'stock/updateStock',
+  async (stock, thunkAPI) => {
+    try {
+      const response = await api.put(`/stocks/${stock.id}`, stock, { withCredentials: true });
+      thunkAPI.dispatch(showNotification(
+        { message: response.data.message, isError: false, isOpen: true },
+      ));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const deleteStock = createAsyncThunk(
+  'stock/deleteStock',
+  async (id, thunkAPI) => {
+    try {
+      const response = await api.delete(`/stocks/${id}`, { withCredentials: true });
+      thunkAPI.dispatch(showNotification(
+        { message: response.data.message, isError: false, isOpen: true },
+      ));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+export {
+  fetchStocks, createStock, updateStock, deleteStock,
+};

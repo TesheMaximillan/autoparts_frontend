8ed45f003e40f480dc4cbbc194e732f4635cfadc
Teshome Kurabachew <dear.teshe@gmail.com>
2022-10-18 22:47:12 +0300

    Create transfer actions

---
+++ src/store/actions/transferActions.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../../api/apiRequests';
+import { hideNotification, showNotification } from '../reducers/uiReducers';
+
+const fetchTransfers = createAsyncThunk(
+  'transfer/fetchTransfers',
+  async (thunkAPI) => {
+    try {
+      const response = await api.get('/transfers', { withCredentials: true });
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const createTransfer = createAsyncThunk(
+  'transfer/createTransfer',
+  async (transfer, thunkAPI) => {
+    try {
+      const response = await api.post('/transfers', transfer, { withCredentials: true });
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const updateTransfer = createAsyncThunk(
+  'transfer/updateTransfer',
+  async (transfer, thunkAPI) => {
+    try {
+      const response = await api.put(`/transfers/${transfer.id}`, transfer, { withCredentials: true });
+      thunkAPI.dispatch(showNotification(
+        { message: response.data.message, isError: false, isOpen: true },
+      ));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const deleteTransfer = createAsyncThunk(
+  'transfer/deleteTransfer',
+  async (id, thunkAPI) => {
+    try {
+      const response = await api.delete(`/transfers/${id}`, { withCredentials: true });
+      thunkAPI.dispatch(showNotification(
+        { message: response.data.message, isError: false, isOpen: true },
+      ));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return response.data;
+    } catch (error) {
+      thunkAPI.dispatch(showNotification({ message: error.message, isError: true, isOpen: true }));
+      setTimeout(() => thunkAPI.dispatch(hideNotification()), 3000);
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  },
+);
+
+export {
+  fetchTransfers, createTransfer, updateTransfer, deleteTransfer,
+};

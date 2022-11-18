ba0cfd68de58b7a00b0d25f8eccaca93b9ec2514
Teshome Kurabachew <dear.teshe@gmail.com>
2022-11-18 15:09:16 +0300

    Modify ListProduct Component

--- src/components/products/ListProduct.jsx
+++ src/components/products/ListProduct.jsx
@@ -1,20 +1,28 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { uuid } from 'uuidv4';
+// import { useSelector } from 'react-redux';
+import { ImPencil2 } from 'react-icons/im';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
 import styles from './ListProduct.module.scss';
 
-const ListProduct = ({ products }) => {
-  const stocks = useSelector((state) => state.stock.stocks);
-  const { container } = styles;
+const ListProduct = ({
+  products, undleUpdate, handleDelete, update,
+}) => {
+  const {
+    container, btns, updateBtn, deleteBtn, disableBtn,
+  } = styles;
+
+  const deleteClass = update ? `${disableBtn}` : deleteBtn;
+
   return (
     <div className={container}>
       <table>
         <thead>
           <tr>
-            <th>No</th>
-            <th>Name</th>
+            <th>Id</th>
+            <th>Product Name</th>
             <th>Part Number</th>
             <th>Category</th>
             <th>Brand</th>
@@ -28,20 +36,20 @@ const ListProduct = ({ products }) => {
         </thead>
         <tbody>
           {products.map((product) => (
-            <tr key={uuid}>
+            <tr key={product.id}>
               <td>{products.indexOf(product) + 1}</td>
               <td>{product.name}</td>
               <td>{product.partNumber}</td>
-              <td>{product.category}</td>
+              <td>{product.categoryName}</td>
               <td>{product.brand}</td>
-              <td>{product.type}</td>
-              <td>{product.cost}</td>
-              <td>{product.selling}</td>
+              <td>{product.status}</td>
+              <td>{(product.cost).toFixed(2)}</td>
+              <td>{(product.selling).toFixed(2)}</td>
               <td>{product.quantity}</td>
-              <td>{stocks.find((stock) => stock.id === product.stock).name}</td>
-              <td>
-                <button type="button">Edit</button>
-                <button type="button">Delete</button>
+              <td>{product.stockName}</td>
+              <td className={btns}>
+                <button type="button" onClick={() => undleUpdate(product.id, product)} className={updateBtn}><ImPencil2 /></button>
+                <button type="button" className={deleteClass} disabled={update} onClick={() => handleDelete(product.id)}><RiDeleteBin5Fill /></button>
               </td>
             </tr>
           ))}

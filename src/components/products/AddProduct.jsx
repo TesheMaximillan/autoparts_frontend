3158ee97ef3d8f49ab3795279d27f5b5a16cedcc
Teshome Kurabachew <dear.teshe@gmail.com>
2022-11-21 22:56:54 +0300

    Modify Add product module

--- src/components/products/AddProduct.jsx
+++ src/components/products/AddProduct.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { PropTypes } from 'prop-types';
 import {
-  createProduct, deleteProduct, fetchStocksProducts, updateProduct,
+  createProduct, deleteProduct, updateProduct,
 } from '../../store/actions/productActions';
 import ListProduct from './ListProduct';
 import Notification from '../common/Notification';
@@ -10,12 +11,49 @@ import ListWrapper from '../common/ListWrapper';
 import FormProduct from './FormProduct';
 import InputWrapper from '../common/InputWrapper';
 import { resetUpdateProduct } from '../../store/reducers/productReducer';
+import { hideNotification, showNotification } from '../../store/reducers/uiReducers';
+import styles from './AddProduct.module.scss';
 
-const AddProduct = () => {
+const { alert } = styles;
+
+const AddProduct = ({ updatedProducts, detailsId }) => {
   const categories = useSelector((state) => state.category.categories);
-  const isOpen = useSelector((state) => state.ui.notification.isOpen);
   const stocks = useSelector((state) => state.stock.stocks);
+  let stocksProducts = useSelector((state) => state.product.stocksProducts);
+
+  if (!stocksProducts) return <div>Loading...</div>;
+
+  stocksProducts = stocksProducts.map((data) => (
+    data.products.map((product) => (
+      {
+        id: product.id,
+        name: product.name,
+        partNumber: product.part_number,
+        brand: product.brand,
+        status: product.status,
+        category: categories.find((category) => category.id === product.category_id).id,
+        categoryName: categories.find((category) => category.id === product.category_id).name,
+        stock: data.stock.id,
+        stockName: data.stock.name,
+        cost: parseFloat(product.cost),
+        selling: parseFloat(product.selling),
+        quantity: parseInt(product.quantity, 10),
+      }
+    )))).flat();
+
+  const [productDetails, setProductDetails] = useState(stocksProducts);
+
+  if (categories.length === 0) {
+    return <div className={alert}>Please Add Category first</div>;
+  }
+
+  if (stocks.length === 0) {
+    return <div className={alert}>Please Add Stock first</div>;
+  }
+
+  const isOpen = useSelector((state) => state.ui.notification.isOpen);
   const products = useSelector((state) => state.product.products);
+
   const productUpdate = useSelector((state) => state.product.productUpdate);
   const id = products.length ? products[products.length - 1].id + 1 : 0;
   const [currentId, setCurrentId] = useState(id);
@@ -23,7 +61,7 @@ const AddProduct = () => {
     stock: stocks[0].name,
     category: categories[0].name,
   });
-  const [productId, setProductId] = useState('');
+  const [productId, setProductId] = useState(0);
   const [update, setUpdate] = useState(productUpdate.update);
   const dispatch = useDispatch();
   const initialState = {
@@ -38,6 +76,22 @@ const AddProduct = () => {
     quantity: '',
   };
 
+  const fetched = (products) => {
+    const newProducts = products.map((product) => ({
+      name: product.name,
+      partNumber: product.part_number,
+      brand: product.brand,
+      status: product.status,
+      category: product.category_id,
+      stock: 1,
+      cost: product.cost,
+      selling: product.selling,
+      quantity: product.quantity,
+    }));
+    return newProducts;
+  };
+
+  const [storeProducts, setStoreProducts] = useState(fetched(products));
   const [product, setProduct] = useState(initialState);
   const [newProducts, setNewProducts] = useState([]);
   const { cost, selling, quantity } = product;
@@ -47,6 +101,10 @@ const AddProduct = () => {
       setProduct(productUpdate.product);
     }
   }, [productUpdate]);
+
+  useEffect(() => {
+    updatedProducts(productDetails);
+  }, [productDetails]);
 
   const undleUpdate = (id, product) => {
     setUpdate(true);
@@ -82,23 +140,41 @@ const AddProduct = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if (update) {
       dispatch(updateProduct(product));
-      dispatch(fetchStocksProducts());
 
       if (cost > 0 && selling >= cost && quantity > 0) {
         setNewProducts(
           newProducts.map((item) => (item.id === productId
             ? { ...product, categoryName: idName.category, stockName: idName.stock } : item)),
         );
+        updatedProducts(
+          productDetails.map((item) => (item.id === detailsId
+            ? { ...product, categoryName: idName.category, stockName: idName.stock } : item)),
+        );
         setUpdate(false);
-        setProductId('');
+        setProductId();
         setProduct(initialState);
         dispatch(resetUpdateProduct());
       }
     } else {
+      if (storeProducts.length) {
+        const duplicate = storeProducts
+          .find((p) => p.partNumber === product.partNumber.toLowerCase()
+        && p.name === product.name.toLowerCase() && p.status === product.status.toLowerCase());
+        if (duplicate) {
+          dispatch(showNotification({
+            message: { error: 'Product already exists' },
+            isError: true,
+            isOpen: true,
+          }));
+          setTimeout(() => dispatch(hideNotification()), 3000);
+          return;
+        }
+      }
+
       dispatch(createProduct(product));
-      dispatch(fetchStocksProducts());
 
       if (cost > 0 && selling >= cost && quantity > 0) {
         setCurrentId(currentId + 1);
@@ -108,6 +184,13 @@ const AddProduct = () => {
           categoryName: idName.category,
           stockName: idName.stock,
         }]);
+        updatedProducts(setProductDetails([...productDetails, {
+          ...product,
+          id: currentId,
+          categoryName: idName.category,
+          stockName: idName.stock,
+        }]));
+        setStoreProducts([...storeProducts, product]);
         setProduct(initialState);
       }
     }
@@ -134,6 +217,11 @@ const AddProduct = () => {
       </ListWrapper>
     </>
   );
+};
+
+AddProduct.propTypes = {
+  updatedProducts: PropTypes.func.isRequired,
+  detailsId: PropTypes.number.isRequired,
 };
 
 export default AddProduct;

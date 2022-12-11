b51e8f498c742b150dea876da5fce026702842a2
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-11 13:56:12 +0300

    Add classname to InputWrapper

--- src/components/common/InputWrapper.jsx
+++ src/components/common/InputWrapper.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './InputWrapper.module.scss';
 
-const InputWrapper = ({ children }) => {
+const InputWrapper = ({ children, classname }) => {
   const { container } = styles;
   return (
-    <div className={container}>{children}</div>
+    <div className={`${container} ${classname}`}>{children}</div>
   );
 };
 
 InputWrapper.propTypes = {
   children: PropTypes.node.isRequired,
+  classname: PropTypes.string.isRequired,
 };
 
 export default InputWrapper;

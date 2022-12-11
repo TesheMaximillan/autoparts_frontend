f5af3fa8d47ef8ca7916632b3ac09ea4a4881e0f
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-11 13:57:48 +0300

    Modify useAdd hook

--- src/components/hooks/useAdd.jsx
+++ src/components/hooks/useAdd.jsx
@@ -4,18 +4,15 @@ import { hideNotification, showNotification } from '../../store/reducers/uiReduc
 
 const useAdd = (props) => {
   const {
-    component, itemUpdate, itemData, items, updatedItems, detailsId,
-    resetUpdateItem, updateItem, createItem, deleteItem,
+    component, itemUpdate, itemData, items, updatedItems,
+    createItem,
   } = props;
 
   const [item, setItem] = useState({ name: '' });
-  const [update, setUpdate] = useState(itemUpdate);
-  const [newItems, setNewItems] = useState([]);
   const [storeItems, setStoreItems] = useState([]);
 
   const id = items.length ? items[items.length - 1].id + 1 : 0;
   const [currentId, setCurrentId] = useState(id);
-  const [itemId, setItemId] = useState();
   const [itemDetails, setItemDetails] = useState(items);
   const { name } = item;
 
@@ -35,31 +32,8 @@ const useAdd = (props) => {
     setItem({ ...item, [e.target.name]: e.target.value });
   };
 
-  const handleUpdate = (id, item) => {
-    setUpdate(true);
-    setItem(item);
-    setItemId(id);
-  };
-
-  const handleDelete = (id) => {
-    setNewItems(newItems.filter((item) => item.id !== id));
-    if (items.find((item) => item.id === id).id) {
-      dispatch(deleteItem(id));
-    }
-  };
-
-  const updateState = () => {
-    setUpdate(false);
-    setNewItems(newItems.map((it) => (it.id === itemId ? item : it)));
-    updatedItems(itemDetails.map((it) => (it.id === detailsId ? item : it)));
-    setItemId();
-    setItem({ name: '' });
-    dispatch(resetUpdateItem());
-  };
-
   const createState = () => {
     setCurrentId(currentId + 1);
-    setNewItems([...newItems, { id: currentId, name }]);
     updatedItems(setItemDetails([...itemDetails, { id: currentId, name }]));
     setStoreItems([...storeItems, { id: currentId, name }]);
     setItem({ name: '' });
@@ -87,19 +61,12 @@ const useAdd = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (update) {
-      dispatch(updateItem(item));
-      updateState();
-    } else {
-      if (checkDuplicate(name)) return;
-      dispatch(createItem(item));
-      createState();
-    }
+    if (checkDuplicate(name)) return;
+    dispatch(createItem(item));
+    createState();
   };
 
-  return {
-    handleChange, handleSubmit, handleUpdate, handleDelete, name, update, newItems,
-  };
+  return { handleChange, handleSubmit, name };
 };
 
 export default useAdd;

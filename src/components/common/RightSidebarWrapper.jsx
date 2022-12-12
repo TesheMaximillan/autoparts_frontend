9747786d6e2e23218d8f4d5b381d9aedead28619
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-12 21:57:59 +0300

    Add autohide feature to rightsidebar component

--- src/components/common/RightSidebarWrapper.jsx
+++ src/components/common/RightSidebarWrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import PropTypes from 'prop-types';
 import styles from './RightSidebarWrapper.module.scss';
@@ -7,6 +7,21 @@ const { container, active } = styles;
 
 const RightSidebarWrapper = ({ children, show, closeSidebar }) => {
   const containerClass = show !== '' ? `${container} ${active}` : container;
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        closeSidebar();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+
   const toggleBtn = (
     <button type="button" className="cancelBtn" onClick={closeSidebar}>
       <AiFillCloseCircle />
@@ -14,7 +29,7 @@ const RightSidebarWrapper = ({ children, show, closeSidebar }) => {
   );
 
   return (
-    <div className={`${containerClass} ${show}`}>
+    <div ref={ref} className={`${containerClass} ${show}`}>
       {toggleBtn}
       {children}
     </div>

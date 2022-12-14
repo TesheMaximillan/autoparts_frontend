ca1f5e87439e408963eb25053a5ae7fe041c3514
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-14 07:25:42 +0300

    Create Product Page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,43 +1,45 @@
 import React, { useState } from 'react';
 import { MdOutlineAddCircle } from 'react-icons/md';
 import { FaProductHunt } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
 import MainBody from '../components/common/MainBody';
 import MainContainer from '../components/common/MainContainer';
 import Sidebar from '../components/common/Sidebar';
 import SubContainer from '../components/common/SubContainer';
 import Topbar from '../components/common/Topbar';
-import AddProduct from '../components/products/AddProduct';
-import ProductDetails from '../components/products/ProductDetails';
-import Navbar from '../components/common/Navbar';
 import styles from './Product.module.scss';
-import Loading from '../components/common/Loading';
+import Navbar2 from '../components/modules/Navbar2';
+import Navbtn from '../components/modules/Navbtn';
+import RightSidebarWrapper from '../components/common/RightSidebarWrapper';
+import Products from '../components/products/Products';
+
+const { icon } = styles;
 
 const Product = () => {
-  const [show, setShow] = useState(true);
-  const { icon } = styles;
-  const products = useSelector((state) => state.product.fetching);
-  const categories = useSelector((state) => state.category.fetching);
-  const stocks = useSelector((state) => state.stock.fetching);
-  const navLinks = [
-    {
-      id: 1, name: 'New Product', icon: <MdOutlineAddCircle className={icon} />, show: true,
-    },
-    {
-      id: 2, name: 'Products', icon: <FaProductHunt className={icon} />, show: false,
-    },
-  ];
-  const [productts, setProductts] = useState();
-  const [storeProducts, setStoreProducts] = useState();
+  const [active, setActive] = useState('');
 
-  const loading = products || categories || stocks;
-
-  const updatedProducts = (product) => {
-    setProductts(product);
+  const handleNav = (nav) => {
+    setActive(nav);
   };
 
-  const storeProductsManagement = (products) => {
-    setStoreProducts(products);
+  const navbtn = [
+    {
+      id: 1,
+      name: 'New Product',
+      icon: <MdOutlineAddCircle className={icon} />,
+      handleClick: handleNav,
+    },
+    {
+      id: 2,
+      name: 'Product Det.',
+      icon: <FaProductHunt className={icon} />,
+      handleClick: handleNav,
+    },
+  ];
+
+  const rightSidebar = () => {
+    if (active === 'Add Product') return '<AddProduct />';
+    if (active === 'Product Det.') return '<ProdcutDetails />';
+    return '';
   };
 
   return (
@@ -45,24 +47,16 @@ const Product = () => {
       <Sidebar />
       <SubContainer>
         <Topbar>
-          <Navbar navLinks={navLinks} setShow={setShow} titleName="Product" iconType={navLinks[1].icon} />
+          <Navbar2 title="Products" icon={navbtn[1].icon}>
+            <Navbtn data={navbtn[0]} />
+            <Navbtn data={navbtn[1]} />
+          </Navbar2>
         </Topbar>
         <MainBody>
-          {loading && <Loading />}
-          {!loading && show
-          && (
-          <AddProduct
-            updatedProducts={updatedProducts}
-            setStoreProducts={storeProductsManagement}
-            storeProducts={storeProducts}
-          />
-          )}
-          {!loading && !show
-          && (
-          <ProductDetails
-            newProducts={productts}
-          />
-          )}
+          <RightSidebarWrapper closeSidebar={handleNav} show={active}>
+            {rightSidebar()}
+          </RightSidebarWrapper>
+          <Products />
         </MainBody>
       </SubContainer>
     </MainContainer>

694303a5a473711ff63c085b09d76a689e5dbfd8
Teshome Kurabachew <dear.teshe@gmail.com>
2022-12-29 16:37:31 +0300

    Modify useAdd custom hook

--- src/components/hooks/useAdd.jsx
+++ src/components/hooks/useAdd.jsx
@@ -1,56 +1,24 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { hideNotification, showNotification } from '../../store/reducers/uiReducers';
 
 const useAdd = (props) => {
   const {
-    component, items, createItem,
+    createItem,
   } = props;
 
   const [item, setItem] = useState({ name: '' });
-  const [storeItems, setStoreItems] = useState([]);
-
-  const id = items.length ? items[items.length - 1].id + 1 : 0;
-  const [currentId, setCurrentId] = useState(id);
   const { name } = item;
 
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
-    setItem({ ...item, [e.target.name]: e.target.value });
-  };
-
-  const createState = () => {
-    setCurrentId(currentId + 1);
-    setStoreItems([...storeItems, { id: currentId, name }]);
-    setItem({ name: '' });
-  };
-
-  const checkDuplicate = (name) => {
-    let result = false;
-    if (storeItems.length) {
-      const duplicate = storeItems.find(
-        (item) => item.name.toLowerCase() === name.toLowerCase().trim(),
-      );
-      if (duplicate) {
-        result = true;
-        dispatch(showNotification({
-          message: { error: `${component} already exists` },
-          isError: true,
-          isOpen: true,
-        }));
-        setTimeout(() => dispatch(hideNotification()), 3000);
-      }
-    }
-    return result;
+    setItem({ ...item, [e.target.name]: e.target.value.toLowerCase().trim() });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    if (checkDuplicate(name)) return;
     dispatch(createItem(item));
-    createState();
+    setItem({ name: '' });
   };
 
   return { handleChange, handleSubmit, name };
